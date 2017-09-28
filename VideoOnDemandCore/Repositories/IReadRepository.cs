fabb044ab42471bd71bbf47cc8e0d55bf80dc4f7
Jonas <N/A>
2017-09-28 13:30:36 +0200

    The GetCourses Method

--- VideoOnDemandCore/Repositories/IReadRepository.cs
+++ VideoOnDemandCore/Repositories/IReadRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace VideoOnDemandCore.Repositories
+﻿using System.Collections.Generic;
+using VideoOnDemandCore.Entities;
+
+namespace VideoOnDemandCore.Repositories
 {
     public interface IReadRepository
     {
+        IEnumerable<Course> GetCourses(string userId);
     }
 }
