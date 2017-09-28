738db3695bc0f9c002c0c0585ab753cef2fd49d5
Jonas <N/A>
2017-09-28 14:48:04 +0200

    Configuring AutoMapper

--- VideoOnDemandCore/Startup.cs
+++ VideoOnDemandCore/Startup.cs
@@ -13,6 +13,8 @@ using VideoOnDemandCore.Data;
 using VideoOnDemandCore.Models;
 using VideoOnDemandCore.Services;
 using VideoOnDemandCore.Repositories;
+using VideoOnDemandCore.Models.DTOModels;
+using VideoOnDemandCore.Entities;
 
 namespace VideoOnDemandCore
 {
@@ -54,6 +56,43 @@ namespace VideoOnDemandCore
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
             services.AddSingleton<IReadRepository, MockReadRepository>();
+
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Video, VideoDTO>();
+
+                cfg.CreateMap<Instructor, InstructorDTO>()
+                    .ForMember(dest => dest.InstructorName,
+                        src => src.MapFrom(s => s.Name))
+                    .ForMember(dest => dest.InstructorDescription,
+                        src => src.MapFrom(s => s.Description))
+                    .ForMember(dest => dest.InstructorAvatar,
+                        src => src.MapFrom(s => s.Thumbnail));
+
+                cfg.CreateMap<Download, DownloadDTO>()
+                    .ForMember(dest => dest.DownloadUrl,
+                        src => src.MapFrom(s => s.Url))
+                    .ForMember(dest => dest.DownloadTitle,
+                        src => src.MapFrom(s => s.Title));
+
+                cfg.CreateMap<Course, CourseDTO>()
+                    .ForMember(dest => dest.CourseId, src =>
+                        src.MapFrom(s => s.Id))
+                    .ForMember(dest => dest.CourseTitle,
+                        src => src.MapFrom(s => s.Title))
+                    .ForMember(dest => dest.CourseDescription,
+                        src => src.MapFrom(s => s.Description))
+                    .ForMember(dest => dest.MarqueeImageUrl,
+                        src => src.MapFrom(s => s.MarqueeImageUrl))
+                    .ForMember(dest => dest.CourseImageUrl,
+                        src => src.MapFrom(s => s.ImageUrl));
+
+                cfg.CreateMap<Module, ModuleDTO>()
+                    .ForMember(dest => dest.ModuleTitle,
+                        src => src.MapFrom(s => s.Title));
+            });
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
