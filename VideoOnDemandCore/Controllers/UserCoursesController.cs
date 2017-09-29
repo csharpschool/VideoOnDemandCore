2058107c0f669078e456868151ab376095f8f68b
Jonas <N/A>
2017-09-29 10:51:44 +0200

    Adding the Index Action and View

--- VideoOnDemandCore/Controllers/UserCoursesController.cs
+++ VideoOnDemandCore/Controllers/UserCoursesController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
 using VideoOnDemandCore.Data;
 using VideoOnDemandCore.Models;
+using VideoOnDemandCore.Models.DTOModels;
 
 namespace VideoOnDemandCore.Controllers
 {
@@ -14,6 +17,23 @@ namespace VideoOnDemandCore.Controllers
         {
             _db = userStore.Context;
             _userStore = userStore;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Courses
+                .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
+                .Select(s => new UserCourseDTO
+                {
+                    CourseId = s.Courses.Id,
+                    CourseTitle = s.Courses.Title,
+                    UserId = s.UserCourses.UserId,
+                    UserEmail =
+                        _userStore.Users.FirstOrDefault(u =>
+                            u.Id.Equals(s.UserCourses.UserId)).Email
+                });
+
+            return View(model);
         }
 
     }
