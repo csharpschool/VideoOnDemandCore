52ed93000ff1066f3b8d77799ad3b9c4239932c4
Jonas <N/A>
2017-09-29 10:54:37 +0200

    Adding the Details Action and View

--- VideoOnDemandCore/Controllers/UserCoursesController.cs
+++ VideoOnDemandCore/Controllers/UserCoursesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using System.Linq;
+using System.Threading.Tasks;
 using VideoOnDemandCore.Data;
 using VideoOnDemandCore.Models;
 using VideoOnDemandCore.Models.DTOModels;
@@ -32,6 +34,34 @@ namespace VideoOnDemandCore.Controllers
                         _userStore.Users.FirstOrDefault(u =>
                             u.Id.Equals(s.UserCourses.UserId)).Email
                 });
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(string userId, int courseId)
+        {
+            if (userId == null || courseId.Equals(default(int)))
+            {
+                return NotFound();
+            }
+
+            var model = await _db.Courses
+                .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
+                .Select(s => new UserCourseDTO
+                {
+                    CourseId = s.Courses.Id,
+                    CourseTitle = s.Courses.Title,
+                    UserId = s.UserCourses.UserId,
+                    UserEmail = _userStore.Users.FirstOrDefault(u =>
+                        u.Id.Equals(s.UserCourses.UserId)).Email
+                })
+                .FirstOrDefaultAsync(w => w.CourseId.Equals(courseId) &&
+                    w.UserId.Equals(userId));
+
+            if (model == null)
+            {
+                return NotFound();
+            }
 
             return View(model);
         }
