60202bd780f21805ae8ba1c4780f929ce79df2ff
Jonas <N/A>
2017-09-29 11:39:34 +0200

    Creating the Tag Helper

---
+++ VideoOnDemandCore/Tag Helpers/ButtonContainerTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VideoOnDemandCore.Tag_Helpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("button-container")]
+    public class ButtonContainerTagHelper : TagHelper
+    {
+        #region Properties
+        public string Controller { get; set; }
+        public string Actions { get; set; }
+        #endregion
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            base.Process(context, output);
+
+            // Replaces <button-container> with <span> tag
+            output.TagName = "span";
+            output.Content.SetHtmlContent("<span style='min-width:100px; display:inline-block;'>");
+
+            var href = "";
+            var action = Actions;
+
+            if (Controller != null && Controller.Length > 0)
+            {
+                if (action != null && action.Length > 0)
+                    href = $@"href='/admin/{Controller}/{action}'";
+                else
+                    href = $@"href='/admin/{Controller}/Index'";
+            }
+
+            output.Content.AppendHtml($@"<a {href}>My link</a>");
+
+            output.Content.AppendHtml("</span>");
+        }
+    }
+
+}
